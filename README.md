9f136d8317f3ff6e1844ae8bd29efe6a35c049f3
Kareem Mohamed <kareem3d.a@gmail.com>
2017-11-13 21:29:19 +0100

    IMPLEMENT CLI INTERFACE
    Command line interface to access logging the commits and changing the dates functions.
    
    Adding README to document the CLI Usage

--- README.md
+++ README.md
@@ -1,3 +1,38 @@
-# GIT change date
+
+git-change-date
+=====
 
 Simple command line to change old commits author and committer dates.
+
+## Installation
+
+```
+npm install -g gcd
+```
+
+## CLI Usage
+Please ensure that you are on the correct directory of the repository you want
+to modify and that there are no unstaged changes before running this.
+
+Usage: `gcd [options]`
+
+Options:
+```
+--path      repository path, defaults to current directory
+--count     Number of commits to show, defaults to 5
+--hash      Search commits with this hash (optional)
+--dev       Show complete error stack (optional)
+```
+
+When you run this command you will be prompted to:
+- Enter commit number in the list
+- Enter the new date **the entered date must be in a format moment can parse**
+https://momentjs.com/docs/#/parsing/string/
+
+After making modifications you will have to force push to remote repositories
+and anyone who has pulled these commits or any future commits will have to reset
+and pull.
+
+## License
+
+MIT @ (Kareem Elbahrawy)[http://www.bitriddler.com]
--- src/index.js
+++ src/index.js
@@ -1,1 +1,138 @@
-export const sum = (a, b) => a + b;
+import minimist from 'minimist';
+import colors from 'colors/safe';
+import prompt from 'prompt';
+import Table from 'cli-table';
+import { getCommits, changeDate } from './git';
+
+const argv = minimist(process.argv.slice(2));
+
+const welcome = () => {
+  console.log(`
+          / /\\            /\\ \\    
+         / /  \\          /  \\ \\   
+        / / /\\ \\        / /\\ \\ \\  
+       / / /\\ \\ \\      / / /\\ \\_\\ 
+      / / /\\ \\_\\ \\    / / /_/ / / 
+     / / /\\ \\ \\___\\  / / /__\\/ /  
+    / / /  \\ \\ \\__/ / / /_____/   
+   / / /____\\_\\ \\  / / /\\ \\ \\     
+  / / /__________\\/ / /  \\ \\ \\    
+  \\/_____________/\\/_/    \\_\\/    
+
+If you don't see formatted output, try to increase width of the terminal to have more space.
+`);
+};
+
+const ask = question => new Promise((resolve, reject) => {
+  console.log('');
+  prompt.get([].concat(question), (err, output) => {
+    if (err) {
+      return reject(err);
+    }
+    console.log('');
+    return resolve(output);
+  });
+});
+
+const logCommits = (commits) => {
+  const table = new Table({
+    head: ['No.', 'Author', 'Subject', 'Date'],
+    chars: {
+      top: '',
+      'top-mid': '',
+      'top-left': '',
+      'top-right': '',
+      bottom: '',
+      'bottom-mid': '',
+      'bottom-left': '',
+      'bottom-right': '',
+      left: '',
+      'left-mid': '',
+      mid: '',
+      'mid-mid': '',
+      right: '',
+      'right-mid': '',
+      middle: ' ',
+    },
+  });
+
+  table.push(...commits.map((commit, index) => ([
+    index + 1,
+    `${commit.name}`,
+    commit.subject.slice(0, 50),
+    commit.date,
+  ])));
+
+  console.log(table.toString());
+};
+
+const logError = (err) => {
+  if (argv.dev) {
+    const table = new Table();
+    table.push([err.stack]);
+    console.log(table.toString());
+  } else {
+    console.log(colors.red(err.message));
+  }
+};
+
+const anotherOne = () => ask({
+  name: 'another',
+  description: 'Choose another commit from the same list (yes/no)?',
+  default: 'no',
+}).then(({ another }) => another.toLowerCase() === 'y' || another.toLowerCase() === 'yes');
+
+const start = (filter) => {
+  let commits;
+  let commit;
+
+  const dirPath = argv.path || process.cwd();
+
+  return getCommits(dirPath, filter)
+    .then((_commits) => {
+      commits = _commits;
+      return logCommits(commits);
+    })
+    .then(() => ask([
+      {
+        name: 'number',
+        description: 'Enter commit number to edit (1, 2, ..etc)',
+        pattern: /[0-9]+/,
+        required: true,
+      },
+      {
+        name: 'date',
+        description: 'Enter the new date',
+        required: true,
+      },
+    ]))
+    .then(({ number, date }) => {
+      if (number > commits.length) {
+        throw new Error(`Number must be between 1 and ${commits.length}`);
+      }
+      commit = commits[number - 1];
+      return changeDate(dirPath, commit.hash, date, date);
+    })
+    .then(() => {
+      console.log(colors.green(`Date changed successfully for commit [${commit.subject}]`));
+    })
+    .then(anotherOne)
+    .then(yes => yes && start(filter))
+    .catch((err) => {
+      logError(err);
+
+      return anotherOne()
+        .then(yes => yes && start(filter));
+    });
+};
+
+welcome();
+
+start({
+  count: argv.count || 5,
+  hash: argv.hash,
+})
+  .catch((err) => {
+    logError(err);
+    return process.exit(0);
+  });
